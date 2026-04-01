3ccc9e320626306d612e4189660e443275e343c7
Felix <gatoab2@gmail.com>
2026-04-01 08:45:06 -0500

    feat:HashLocationStrategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,14 +9,19 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (
-  http: HttpClient
+  http: HttpClient,
 ) => new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
     provideHttpClient(),
     importProvidersFrom([
       TranslateModule.forRoot({
